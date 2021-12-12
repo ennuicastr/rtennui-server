284ea711b7593968330a05a65eb61159e87ab5c4
Yahweasel <git-rpo@yahweasel.com>
2021-12-12 14:40:24 -0500

    Support for video in the basic client

--- samples/basic-server/root/basic-client.js
+++ samples/basic-server/root/basic-client.js
@@ -9,7 +9,7 @@
         await ac.resume();
     }
 
-    const ms = await navigator.mediaDevices.getUserMedia({audio: true});
+    const ms = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
 
     const url = new URL(document.location.href);
     const room = url.searchParams.get("room") || "RTEnnui";
@@ -18,6 +18,20 @@
     await RTEnnui.load();
 
     const conn = new RTEnnui.Connection(ac);
+
+    conn.on("track-started-video", ev => {
+        Object.assign(ev.element.style, {
+            width: "640px",
+            height: "360px"
+        });
+        document.body.appendChild(ev.element);
+    });
+
+    conn.on("track-ended-video", ev => {
+        try {
+            ev.element.parentElement.removeChild(ev.element);
+        } catch (ex) {}
+    });
 
     conn.on("track-started-audio", ev => {
         ev.node.connect(ac.destination);
@@ -35,4 +49,9 @@
 
     const audio = await RTEnnui.createAudioCapture(ac, ms);
     conn.addAudioTrack(audio);
+
+    if (ms.getVideoTracks().length) {
+        const video = await RTEnnui.createVideoCapture(ms);
+        conn.addVideoTrack(video);
+    }
 })();
